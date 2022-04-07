03b087cde4b451079cce4d287103e9f867ac48fd
voronenok11 <rodion140203@gmail.com>
2022-04-07 23:52:26 +0300

    файлы под классы перенесены в отдельную папку lib

---
+++ lib/RedGhost.py
@@ -0,0 +1,9 @@
+class RedGhost:
+    position = (11, 13)
+    orientation = (0, 0)
+    tactic = "not activated"
+    def ChangeTactic(self, board, pacman):
+        pass
+    def Move(self, board, pacman):
+        self.ChangeTactic(board, pacman)
+            
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from Board import Board
-from RedGhost import RedGhost
-from BlueGhost import BlueGhost
-from PinkGhost import PinkGhost
-from YellowGhost import YellowGhost
-from Pacman import Pacman
+from lib.Board import Board
+from lib.RedGhost import RedGhost
+from lib.BlueGhost import BlueGhost
+from lib.PinkGhost import PinkGhost
+from lib.YellowGhost import YellowGhost
+from lib.Pacman import Pacman
 board = Board()
 red_ghost = RedGhost()
 blue_ghost = BlueGhost()
